2fe76dd68f425f463652b73f26a3801a9ec4dd4e
PedroDuarte <p.fmarket1996@gmail.com>
2024-10-24 23:45:56 -0300

    Implementando paginação com Pageable e Page<T>

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -4,6 +4,9 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -26,17 +29,28 @@ public class MedicoController {
         medicoRepository.save(new Medico(dados));
     }
 
-    @GetMapping("/lista")
-    public List<DadosRetornoMedico> listarMedicos(){
+//    @GetMapping("/lista")
+//    public List<DadosRetornoMedico> listarMedicos(){
+//
+//        /*Explicando cada parte do código abaixo, a partir de .stream() o .stream() transforma uma lista para o tipo Stream, para podermos usufruir dos métodos dessa classe
+//         sendo assim, utilizaremos o .map() que é um método que pega cada elemento da lista passada e transforma em outro elemento, de acordo com o que for pedido
+//          no caso abaixo ele pegou o construtor da record DadosCadastroMedico::new que recebe um tipo Medico que é o que o repository aceita e
+//          finaliza, transformando de volta em uma lista com o .toList()*/
+//        return medicoRepository.findAll().stream().map(DadosRetornoMedico::new).toList();
+//    }
 
-        /*Explicando cada parte do código abaixo, a partir de .stream() o .stream() transforma uma lista para o tipo Stream, para podermos usufruir dos métodos dessa classe
-         sendo assim, utilizaremos o .map() que é um método que pega cada elemento da lista passada e transforma em outro elemento, de acordo com o que for pedido
-          no caso abaixo ele pegou o construtor da record DadosCadastroMedico::new que recebe um tipo Medico que é o que o repository aceita e
-          finaliza, transformando de volta em uma lista com o .toList()*/
-        return medicoRepository.findAll().stream().map(DadosRetornoMedico::new).toList();
+    /*Agora no metodo de recuperação da lista usaremos o tipo Page<T> e como parametro usaremos a Classe Pageable do Spring, para poder manipular a quantidade de páginas mostradas,
+    * ou a quantidade de dados por página, assim sucessivamente, retiramos o stream() pois usando o Pageable conseguimos usar esse recurso do .map()
+    * IMPORTANTE com os recursos de Pageable a manipulação de pagina e dados como ordenação por exemplo, fica a critério do cliente, passando an URL(size, page, sort=nomeDoCampo,desc)
+    * A Anotação @PageableDefault serve para caso o cliente não implemente na url, quando consumir a endpoint*/
+
+    @GetMapping("/lista")
+    public Page<DadosRetornoMedico> listarMedicos(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+        return medicoRepository.findAll(paginacao).map(DadosRetornoMedico::new);
+
     }
 
-//  '  @GetMapping("/individual/{id}")
+//    @GetMapping("/individual/{id}")
 //    public Medico getMedicoId(@PathVariable("id") String id){
 //         return medicoRepository.findById()
 //    }
